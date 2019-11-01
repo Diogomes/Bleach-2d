a295f3f5782eeb92b0620b1a9bcb3aa1fb7b7bc4
Diogo <40742985+Diogomes@users.noreply.github.com>
2019-11-01 10:50:16 -0300

    Update Bleach1.py

--- Bleach1.py
+++ Bleach1.py
@@ -2,7 +2,7 @@ import pygame as pyg
 
 def main():
     pyg.init()
-    tela = pyg.display.set_mode([500, 500])
+    tela = pyg.display.set_mode([700, 800])
     pyg.display.set_caption("Bleach Inicio")
     relogio = pyg.time.Clock()
     cor_branca = (255,255,255)
@@ -19,34 +19,36 @@ def main():
         for event in pyg.event.get():
             if event.type == pyg.QUIT:
                 sair = True
-            #if event.type ==pyg.MOUSEBUTTONDOWN:
-                
-'''
-Butões interação com o personagem
-'''
+            #if event.type ==pyg.MOUSEBUTTONDOWN:            
+
             if event.type == pyg.KEYDOWN:
                 if event.key == pyg.K_LEFT:
                     ret.move_ip(-10,0)
+                else:
+                    ret.move_ip(0,0)
                 if event.key == pyg.K_RIGHT:
                     ret.move_ip(10,0)
                 if event.key == pyg.K_UP:
                     ret.move_ip(0,-10)
                 if event.key == pyg.K_DOWN:
                     ret.move_ip(0,10)
-                #if event.key == pyg.K_w:
-                    #ret.move_ip(0,10)
-                #if event.key == pyg.K_a:
-                    #ret.move_ip(0,10)
-                #if event.key == pyg.K_d:
-                    #ret.move_ip(0,10)
-                #if event.key == pyg.K_s:
-                    #ret.move_ip(0,10)
+                if event.key == pyg.K_w:
+                    ret.move_ip(0,-10)
+                if event.key == pyg.K_a:
+                    ret.move_ip(-10,0)
+                if event.key == pyg.K_d:
+                    ret.move_ip(10,0)
+                if event.key == pyg.K_s:
+                    ret.move_ip(0,10)
                 #if event.key == pyg.K_j:
                     #ret.move_ip(0,10)
                 #if event.key == pyg.K_i:
                     #ret.move_ip(0,10)
                 #if event.key == pyg.K_l:
                     #ret.move_ip(0,10)
+            #if event.type == pyg.KEYUP:
+                #if event.key == pyg.K_LEFT:
+                    #ret.move_ip(0,0)
         relogio.tick(28)
         tela.fill(cor_branca)
         tela.blit(sup, (50, 50))
