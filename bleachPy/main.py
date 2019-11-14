bde7e0f83aebc544101593145611fb4872beee6c
alexandrelolzinh0123 <alexandrehenriquenascimento123@gmail.com>
2019-11-14 00:55:21 -0300

    Add files via upload

---
+++ bleachPy/main.py
@@ -0,0 +1,95 @@
+import pygame
+pygame.init()
+
+win = pygame.display.set_mode((500,480))
+
+pygame.display.set_caption("First Game")
+
+walkRight = [pygame.image.load('sprites/ichigo/ichigoWalk_17.png'), pygame.image.load('sprites/ichigo/ichigoWalk_18.png'), pygame.image.load('sprites/ichigo/ichigoWalk_19.png'), pygame.image.load('sprites/ichigo/ichigoWalk_20.png'), pygame.image.load('sprites/ichigo/ichigoWalk_21.png'), pygame.image.load('sprites/ichigo/ichigoWalk_22.png'), pygame.image.load('sprites/ichigo/ichigoWalk_23.png'), pygame.image.load('sprites/ichigo/ichigoWalk_24.png'), pygame.image.load('sprites/ichigo/ichigoWalk_25.png')]
+walkLeft = [pygame.image.load('sprites/ichigo/ichigoWalk_17.png'), pygame.image.load('sprites/ichigo/ichigoWalk_18.png'), pygame.image.load('sprites/ichigo/ichigoWalk_19.png'), pygame.image.load('sprites/ichigo/ichigoWalk_20.png'), pygame.image.load('sprites/ichigo/ichigoWalk_21.png'), pygame.image.load('sprites/ichigo/ichigoWalk_22.png'), pygame.image.load('sprites/ichigo/ichigoWalk_23.png'), pygame.image.load('sprites/ichigo/ichigoWalk_24.png'), pygame.image.load('sprites/ichigo/ichigoWalk_25.png')]
+bg = pygame.image.load('sprites/ichigo/bg.png')
+char = pygame.image.load('sprites/ichigo/ichigo_stance_1.png')
+
+clock = pygame.time.Clock()
+
+
+class player(object):
+    def __init__(self,x,y,width,height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+        self.jumpCount = 10
+
+    def draw(self, win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            win.blit(walkLeft[self.walkCount//3], (self.x,self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(walkRight[self.walkCount//3], (self.x,self.y))
+            self.walkCount +=1
+        else:
+            win.blit(char, (self.x,self.y))
+
+
+
+def redrawGameWindow():
+    win.blit((0,0,0), (0,0))
+    man.draw(win)
+    
+    pygame.display.update()
+
+
+#mainloop
+man = player(200, 410, 64,64)
+run = True
+while run:
+    clock.tick(27)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    keys = pygame.key.get_pressed()
+
+    if keys[pygame.K_LEFT] and man.x > man.vel:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
+    elif keys[pygame.K_RIGHT] and man.x < 500 - man.width - man.vel:
+        man.x += man.vel
+        man.right = True
+        man.left = False
+    else:
+        man.right = False
+        man.left = False
+        man.walkCount = 0
+        
+    if not(man.isJump):
+        if keys[pygame.K_SPACE]:
+            man.isJump = True
+            man.right = False
+            man.left = False
+            man.walkCount = 0
+    else:
+        if man.jumpCount >= -10:
+            neg = 1
+            if man.jumpCount < 0:
+                neg = -1
+            man.y -= (man.jumpCount ** 2) * 0.5 * neg
+            man.jumpCount -= 1
+        else:
+            man.isJump = False
+            man.jumpCount = 10
+            
+    redrawGameWindow()
+
+pygame.quit()
